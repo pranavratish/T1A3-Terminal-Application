bd7a220ee152cf9d764d0e59f6b68ec1b8855600
Pranav Ratish <pranavratish@gmail.com>
2021-07-13 13:57:52 +1000

    Implemented  Encryption function and Validation function

---
+++ src/functions.py
@@ -0,0 +1,36 @@
+from cryptography.fernet import Fernet
+import json
+
+def secure():
+    key = Fernet.generate_key()
+
+    info = input('What would you like to encrypt?:\n')
+    data = info.encode()
+
+    f = Fernet(key)
+    encrypted = f.encrypt(data)
+
+    s_encrypted = encrypted.decode()
+
+    with open('src/storage.json', 'r') as s:
+        d = json.load(s)
+        d[s_encrypted] = info
+
+    with open('src/storage.json', 'w') as s:
+        s.write(json.dumps(d))
+
+    print(encrypted)
+
+def validKey():
+    key_input = input('Please enter the key associated with the information you are trying to access:\n')
+    key_input = str(key_input)
+
+    k = open('src/storage.json')
+
+    k_pass = json.load(k)
+
+    if key_input in k_pass:
+        print(k_pass[key_input])
+    else:
+        print('Invalid Key.\nPlease Try Again.')
+
