5221d9281ae224d6800bad91b30340750c18d2df
Pranav Ratish <pranavratish@gmail.com>
2021-07-14 15:50:36 +1000

    New User function

--- src/functions.py
+++ src/functions.py
@@ -26,7 +26,6 @@ def validKey():
     key_input = str(key_input)
 
     k = open('src/storage.json')
-
     k_pass = json.load(k)
 
     if key_input in k_pass:
@@ -51,7 +50,7 @@ def delete():
     objDel = input('Which key would you like to delete:\n')
     objDel = str(objDel)
 
-    confirm = input('Are you sure about this? You won\'t be able to get this information back, if you are sure please press \'Y\':\n')    
+    confirm = input('Are you sure about this? You won\'t be able to get this information back once deleted.\nIf you are sure please press \'Y\':\n')    
 
     if confirm == 'Y':
         with open('src/storage.json', 'r') as s:
@@ -62,4 +61,37 @@ def delete():
             s.write(json.dumps(d))
     else:
         return
+
+def newUser():
+    user = input('Please enter a unique username:\n')
+    user = str(user)
+
+    if user.isalpha() != True:
+        print('That was an invalid username.\nPlease ensure that you use only letters in your username and try again.')
+        return
+
+    pass_ = input('Please enter a secure password:\n')
+    pass_ = str(pass_)
+
+    acc_u = {}
+    acc_u['Username'] = user
+
+    acc_p = {}
+    acc_p['Password'] = pass_
+
+    usr = open('src/users.json')
+    usr_pass = json.load(usr)
+
+    if usr_pass.count(acc_u) > 0:
+        print('There is already a user with this name.\nPlease try again.')
+        return
+    else:
+        with open('src/users.json', 'r') as u:
+            l = json.load(u)
+            l.append(acc_u)
+            l.append(acc_p)
+
+        with open('src/users.json', 'w') as u:
+            u.write(json.dumps(l))
+
 
